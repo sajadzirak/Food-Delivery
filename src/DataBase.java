a116a358c8fe5e6580ffd1b960765f2d010a5edf
sajadzirak <iriafmig29@gmail.com>
2023-05-26 10:16:40 +0330

    database class created

---
+++ src/DataBase.java
@@ -0,0 +1,46 @@
+import javafx.collections.ObservableList;
+import java.io.*;
+
+public class DataBase {
+    
+    private ObservableList<Restaurant> restaurantList;
+    private static String password = "123";
+
+    public DataBase() throws ClassNotFoundException, IOException{
+        readRestaurants();
+    }
+    
+    public ObservableList<Restaurant> getRestaurantList() {
+        return restaurantList;
+    }
+
+    public void setRestaurantList(ObservableList<Restaurant> restaurantList) {
+        this.restaurantList = restaurantList;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    public static void setPassword(String newPassword) {
+        password = newPassword;
+    }
+    
+
+    public void readRestaurants() throws IOException, ClassNotFoundException{
+        FileInputStream restaurantF = new FileInputStream("DB/files/restaurants.dat");
+        ObjectInputStream input = new ObjectInputStream(restaurantF);
+        restaurantList =  (ObservableList<Restaurant>) input.readObject();
+        input.close();
+    }
+
+    public void writeRestaurants() throws IOException{
+        FileOutputStream restaurantF = new FileOutputStream("DB/files/restaurants.dat");
+        ObjectOutputStream output = new ObjectOutputStream(restaurantF);
+        output.writeObject(restaurantList);
+        output.close();
+    }
+
+
+
+}
---
+++ src/Server.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Server {
+    
+    
+    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    }
+
+
+}
