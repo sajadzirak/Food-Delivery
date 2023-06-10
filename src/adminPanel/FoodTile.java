7db6396ef881cd1bea32f55acf85404105a28084
sajadzirak <iriafmig29@gmail.com>
2023-06-10 19:31:58 +0330

    it is elon musk

--- src/adminPanel/FoodTile.java
+++ src/adminPanel/FoodTile.java
@@ -1,13 +1,23 @@
 package adminPanel;
 
+import java.io.File;
+
+import javafx.scene.image.Image;
+import server.src.DataBase;
 import server.src.Food;
 
 public class FoodTile extends Tile{
     
-    Food food;
+    private Food food;
     
-    public FoodTile(Food food){
-        this.food = food;
+    public FoodTile(Food f){
+        super();
+        food = f;
+        File file = new File(food.getFoodImagePath());
+        imageView.setImage(new Image(DataBase.imageAbsolutePath+file.getName()));
+        nameLabel.setText(food.getFoodName());
+        typeLabel.setText(food.getFoodType().name());
+        layout.getChildren().addAll(imageLabel, nameLabel, typeLabel, buttonBox);
     }
     
 }
